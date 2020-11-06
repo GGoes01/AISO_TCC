a9874b3c75b832d495549b8a32a5fcf797bd0885
GGoes01 <34575335+GGoes01@users.noreply.github.com>
2020-11-06 16:31:04 -0300

    06-11-2020
    Correção da verificação de repetição
    Falta adicionar e corrigir a verificação de validade
    Melhor valor ~38000

--- Testes.py
+++ Testes.py
@@ -1,13 +1,8 @@
-from AISO import mutacionar, clonar
-from Mapa import Mapa
-from Populacao import Populacao
-import pandas as pd
-import numpy as np
 from AISO import Aiso
 
-aiso = Aiso('paths/kroA100.tsp', num_ger=200, num_cel=5, num_clones=500, validade=10)
+aiso = Aiso('paths/kroA100.tsp', num_ger=250, num_cel=5, num_clones=50, validade=5)
 aiso.executar()
 
-# falta implementar a validade
-# restringir a quantidade de trocas
+# Falta corrigir a validade
+# Falta comentar TUDO
 
