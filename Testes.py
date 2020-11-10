c771089c97f1661bb66d655adfb9a72c62978b05
GGoes01 <34575335+GGoes01@users.noreply.github.com>
2020-11-10 18:39:22 -0300

    10_11_20
    Avaliação de validade aplicada
    Melhor resultado: ~36200

--- Testes.py
+++ Testes.py
@@ -1,8 +1,19 @@
 from AISO import Aiso
 
-aiso = Aiso('paths/kroA100.tsp', num_ger=250, num_cel=5, num_clones=50, validade=5)
+aiso = Aiso('paths/kroA100.tsp', num_ger=500, num_cel=10, num_clones=150, validade=3)
 aiso.executar()
 
 # Falta corrigir a validade
 # Falta comentar TUDO
 
+'''
+250, 5, 100 - 37948.021
+250, 5, 100 - 43293.418
+250, 5, 125 - 44819.489
+250, 5, 75 - 39216.513
+250, 5, 75 - 40728.226
+250, 5, 75 - 41073.638
+250, 5, 75 - 42082.060
+500, 5, 75 - 36201.949
+250, 3, 75 - 42407.352
+'''
